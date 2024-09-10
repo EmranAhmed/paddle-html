0a53e48ed9d46d0b5010de4d7c51960bc0ad612a
Emran Ahmed <emran.bd.08@gmail.com>
2024-09-10 16:51:47 +0600

    update

--- webhook.php
+++ webhook.php
@@ -1,10 +1,5 @@
 <?php
 
-
-function log_it( $message ) {
-	$log_dir = dirname( __FILE__ ) . "/debug.txt";
-
-	file_put_contents( $log_dir, $message, FILE_APPEND );
-}
-
-log_it( 'REQUEST:' . print_r( $_REQUEST, 1 ) . "\n\n\n" );
+$log_dir = dirname( __FILE__ ) . "/debug.txt";
+$data = print_r( $_REQUEST, 1 );
+file_put_contents( $log_dir, $data, FILE_APPEND );
